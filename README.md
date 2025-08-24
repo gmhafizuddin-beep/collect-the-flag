b4d82adf99a7e65820d27390f582dee992275e0d
gmhafizuddin-beep <gmhafizuddin@gmail.com>
2025-08-24 13:45:37 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://gmhafizuddin-beep.github.io/collect-the-flag/](https://gmhafizuddin-beep.github.io/collect-the-flag/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/gmhafizuddin-beep/collect-the-flag** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/gmhafizuddin-beep/collect-the-flag** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ tilemap.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010000000001010100000001000000010100000000000101000001010000000101000000000000010000010000000101010000000000000100000100000001010100000000000001000001000000010101000000010101010000010000000001010000000101010100000100000000010100000000000000000000000001010101000000000000000000000000000101010000000000000000000001010101010100000000000000000001010100000101000000000001000000000000000001010000000000010000000000000000010100000000000100000000000000000101010101010101010101010101010101`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . 2 2 2 . . . 2 . . . 2 
+2 . . . . . 2 2 . . 2 2 . . . 2 
+2 . . . . . . 2 . . 2 . . . . 2 
+2 . . . . . . 2 . . 2 . . . . 2 
+2 . . . . . . 2 . . 2 . . . . 2 
+2 . . . 2 2 2 2 . . 2 . . . . 2 
+2 . . . 2 2 2 2 . . 2 . . . . 2 
+2 . . . . . . . . . . . . 2 2 2 
+2 . . . . . . . . . . . . . 2 2 
+2 . . . . . . . . . . 2 2 2 2 2 
+2 . . . . . . . . . 2 2 2 . . 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 . . . . . 2 . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile0":
+            case "tile2":return tile2;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
